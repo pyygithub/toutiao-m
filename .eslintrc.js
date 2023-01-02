10426a321e93d2d93f67e434e0eeb8929f1d95cc
panyy41 <panyy41@midea.com>
2023-01-02 22:16:00 +0800

    引入iconfont和移动端适配插件

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,8 +13,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-     // 取消文件和变量的驼峰命名
-     'vue/multi-word-component-names': 0,
-     camelcase: 'off'
+    // 取消文件和变量的驼峰命名
+    'vue/multi-word-component-names': 0,
+    camelcase: 'off'
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
 // 加载全局样式
 import './styles/index.less'
+
+// 加载vant组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 自动设置rem基准值（html font-Size大小)
+import 'amfe-flexible'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
